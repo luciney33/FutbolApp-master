b8a38e1ce802873d24c426a2b9eeac48a6a15359
luciney33 <lucia.penafiel@educa.madrid.org>
2025-05-26 10:44:39 +0200

    siu

--- src/main/java/org/example/ui/App.java
+++ src/main/java/org/example/ui/App.java
@@ -3,8 +3,12 @@ package org.example.ui;
 import org.example.common.ExcepcionIdErroneo;
 
 public class App {
-    public static void main(String[] args) throws ExcepcionIdErroneo {
-        new EntradaSalida().menuPrincipal();
+    public static void main(String[] args)  {
+        try {
+            new EntradaSalida().menuPrincipal();
+        } catch (ExcepcionIdErroneo e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
